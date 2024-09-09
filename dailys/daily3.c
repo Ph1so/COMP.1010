1c61393b5b7ff178af884bb29c584b4430c4585e
Phi Nguyen <phiwe3296@gmail.com>
2024-09-09 10:11:45 -0400

    daily 3

---
+++ dailys/daily3.c
@@ -0,0 +1,16 @@
+/*
+Author: Phi Nguyen
+Date: 09/09/2024
+Output:
+
+Hello, my name is Phi Nguyen. This class is "Awesome Sauce"!
+Something about me: I am a 3D artist on Blender!
+
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, my name is Phi Nguyen. This class is \"Awesome Sauce\"!\nSomething about me: I am a 3D artist on Blender!\n");
+    return 0;
+}
