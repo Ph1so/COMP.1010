82e5bdd71b4efccf3f91af775a29362d5784137d
Phi Nguyen <phiwe3296@gmail.com>
2024-09-26 11:06:42 -0400

    qeustions

--- dailys/daily10.c
+++ dailys/daily10.c
@@ -1,13 +1,14 @@
 /*
 Author: Phi Nguyen
 Date: 09/27/2024
-Answer to the question:
+Purpose:
 
-Printing many times does not cause a stack overflow like recursion did with large numbers because
-printing does not use a frame of memory and it does not stack like the recursion did. The recurison in daily6
-kept stacking frames because each function call could not be complete until the very last function call returned.
-Thus, each function call added to the stack in memory, eventually causing a stack overflow. Printing does not
-follow the same nature of stacking because it is a SINGLE function call.
+Repeatedly ask the user to input a negative number until the user inputs a correct value.
+
+Time spent:
+
+8 minutes
+
 
 */
 
